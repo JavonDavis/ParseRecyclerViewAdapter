d7b276fa4c6fc58c51d63abae2979b70c79885c1
Javon Davis <javonldavis14@gmail.com>
2016-01-12 09:25:43 -0500

    added pagination in attempt to resolve #1, still needs more testing

---
+++ example/src/main/java/com/javon/example/CustomQueryAdapter.java
@@ -0,0 +1,28 @@
+package com.javon.example;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+import com.javon.parserecyclerviewadapter.ParseRecyclerQueryAdapter;
+import com.parse.ParseObject;
+
+/**
+ * @author Javon Davis
+ *         Created by Javon Davis on 11/01/16.
+ */
+public class CustomQueryAdapter extends ParseRecyclerQueryAdapter<ParseObject, DefaultViewHolder> {
+
+    public CustomQueryAdapter(Context context, Class clazz, String className) {
+        super(context, clazz, className);
+    }
+
+    public CustomQueryAdapter(Context context, Class clazz,QueryFactory queryFactory) {
+        super(context, clazz,queryFactory);
+    }
+
+    @Override
+    public void onBindViewHolder(DefaultViewHolder holder, int position) {
+        super.onBindViewHolder(holder, position);
+        holder.staticTitle.setText(object.getString("title").substring(0,2)+"...");
+    }
+}
